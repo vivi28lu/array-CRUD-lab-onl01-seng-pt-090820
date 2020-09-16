71d76c3c4f90c502cfa5b9c4b11e1868796d1e78
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:40:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  foods = ["wow", "pasta", "am", "fruit", "chips"]
+  foods = ["pasta", "am", "fruit", "chips"]
 end
 
 def add_element_to_end_of_array(array, element)
