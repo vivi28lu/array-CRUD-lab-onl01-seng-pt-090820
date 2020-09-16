0ec36c28a5ced2599beea1ab2942ef69e6d81ca0
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:35:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  foods = ["pasta", "pizza", "fruit", "chips", "arrays!"]
+  wow_foods = foods.shift
 end
 
 def retrieve_element_from_index(array, index_number)
