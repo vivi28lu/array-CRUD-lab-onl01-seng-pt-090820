4ec45401ba75e5147ac6cb539ac497d1bd231239
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:41:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,5 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
+  foods[-1]
 end
