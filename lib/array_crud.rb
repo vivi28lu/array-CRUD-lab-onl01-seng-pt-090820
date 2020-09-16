47a96037fcfd3e242b845d08342146388adc80bf
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:41:43 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  foods = ["pasta", "am", "fruit", "chips"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips"]
+  foods << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips"]
+  foods.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
+  arrays_foods = foods.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
+  wow_foods = foods.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  foods = ["pasta", "am", "fruit", "chips", "arrays!"]
+  foods[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
+  foods[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
+  foods[-1]
 end
