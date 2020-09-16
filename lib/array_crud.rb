7e9d4db8e1844700ac3b74706e48e37d0a7e2e2d
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:39:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,31 +3,31 @@ def create_an_empty_array
 end
 
 def create_an_array
-  foods = ["pasta", "pizza", "fruit", "chips"]
+  foods = ["wow", "pasta", "am", "fruit", "chips"]
 end
 
 def add_element_to_end_of_array(array, element)
-  foods = ["pasta", "pizza", "fruit", "chips"]
+  foods = ["wow", "pasta", "am", "fruit", "chips"]
   foods << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  foods = ["pasta", "pizza", "fruit", "chips"]
+  foods = ["wow", "pasta", "am", "fruit", "chips"]
   foods.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  foods = ["pasta", "pizza", "fruit", "chips", "arrays!"]
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
   arrays_foods = foods.pop
 end
 
 def remove_element_from_start_of_array(array)
-  foods = ["wow", "pasta", "pizza", "fruit", "chips", "arrays!"]
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
   wow_foods = foods.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  foods = ["pasta", "am", "fruit", "chips", "arrays!"]
+  foods = ["wow", "pasta", "am", "fruit", "chips", "arrays!"]
   foods[1]
 end
 
