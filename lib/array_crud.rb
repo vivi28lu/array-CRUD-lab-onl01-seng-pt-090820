1a00099dae08771cde2094fbf8569527d863f2af
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:30:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   foods = ["pasta", "pizza", "fruit", "chips"]
-  foods << "arrays"
+  foods << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
