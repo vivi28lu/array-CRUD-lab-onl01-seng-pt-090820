7a1d4bb73fdcedc5e552c214a10d36be299150fa
Viviana Lugo <vivi28lu@gmail.com>
2020-09-16 00:32:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  foods = []
+  foods = ["pasta", "pizza", "fruit", "chips"]
+  foods.unshift ("bbq")
 end
 
 def remove_element_from_end_of_array(array)
